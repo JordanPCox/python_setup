cd849fe967b8980c260e69a8600c5944fee22ae0
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-04-28 13:26:47 -0400

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,17 @@
+def hello():
+    print("Greetings, Earthling!")
+
+hello()
+
+def pack(item1, item2, item3):
+    return [item1, item2, item3]
+result = pack("wallet", "phone", "keys")
+print(result)
+
+def eat_lunch(lunchbox_contents):
+    if lunchbox_contents:
+        print(f"I'm going to eat {lunchbox_contents[0]}")
+        for item in lunchbox_contents[1:]:
+            print(f"Next I eat {item}")
+
+eat_lunch(["apple", "banana", "carrot"])
